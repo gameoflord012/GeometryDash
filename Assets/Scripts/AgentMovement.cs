fe71ee18c128ace2568a17d3458d459ee4973c10
gameoflord012 <manhhieu012@gmail.com>
2021-09-27 16:19:15 +0700

    project setup 4

---
+++ Assets/Scripts/AgentMovement.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class AgentMovement : MonoBehaviour
+{
+    [SerializeField] float movementSpeed = 5f;
+
+    Rigidbody2D rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        rb.velocity = new Vector2(movementSpeed, rb.velocity.y);
+    }
+}
