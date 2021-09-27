94f84e2ac6ffe4dd5dc2067216506ee7d66b86ff
gameoflord012 <manhhieu012@gmail.com>
2021-09-27 17:56:37 +0700

    jump point and fix jump

--- Assets/Scripts/AgentMovement.cs
+++ Assets/Scripts/AgentMovement.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,7 +9,8 @@ public class AgentMovement : MonoBehaviour
     [SerializeField] float movementSpeed = 10f;
     [SerializeField] float jumpForce = 15f;
     [SerializeField] float onGroundDistance = 1f;
-    [SerializeField] float rotationSpeed = 30f;    
+    [SerializeField] float jumpPointDistance = 1f;
+    [SerializeField] float rotationSpeed = 30f;
 
     Rigidbody2D rb;
 
@@ -24,21 +26,28 @@ public class AgentMovement : MonoBehaviour
 
     public void Jump()
     {
-        if(IsOnGround())
+        if(IsOnGround() || NearJumpPoint())
         {
-            rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+            rb.velocity = Vector2.up * jumpForce;
             rb.AddTorque(rotationSpeed, ForceMode2D.Impulse);
         }
-    }
+    }    
 
     bool IsOnGround()
     {
         var hit = Physics2D.Raycast(transform.position, Vector2.down, onGroundDistance, 1 << LayerMask.NameToLayer("Platform"));
         return hit.collider != null;
+    }    
+
+    private bool NearJumpPoint()
+    {
+        var hit = Physics2D.CircleCast(transform.position, jumpPointDistance, Vector2.zero, 0, 1 << LayerMask.NameToLayer("Interactable"));        
+        return hit.collider != null && hit.collider.GetComponent<JumpPoint>() != null;
     }
 
     private void OnDrawGizmosSelected()
     {
         Gizmos.DrawRay(transform.position, Vector2.down * onGroundDistance);
+        Gizmos.DrawWireSphere(transform.position, jumpPointDistance);
     }
 }
